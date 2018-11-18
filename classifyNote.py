00e0156228409f2d309432fee611784655bc78fb
Bartlomiej Szal <bartlomiej.szal@gmail.com>
2018-11-18 14:14:42 +0100

    Added more elaborate notes classification

--- classifyNote.py
+++ classifyNote.py
@@ -1,4 +1,12 @@
-def classifyNote(value):
+import numpy as np
+def isFilled(noteCenter, threshold):
+    (height, width) = np.shape(noteCenter)
+    fill = 1 - (sum(sum(noteCenter))/(width * height))
+    if fill > threshold:
+        return True
+    return False
+
+def classifyNote(value, noteImage):
     notes = {
         0: "D",
         0.5: "E",
@@ -13,4 +21,30 @@ def classifyNote(value):
         5: "G",
         5.5: "A"
     }
-    return notes.get(value, "Invalid note")
+    value = notes.get(value, "Invalid note")
+    (height, width) = np.shape(noteImage)
+    if height/width > 1.3 or height > 30:
+        # staffed
+        staff = " Staff"
+        rightEdge = noteImage[:, int(width/5)*4:]
+        if isFilled(rightEdge, 0.6):
+            tail = " No tail"
+            noteCenter = noteImage[int(height/12)*9 : height - int(height/12), int(width/4) : width - int(width/4)]
+        else:
+            tail = " Tail"
+            noteCenter = noteImage[int(height/12)*9 : height - int(height/12), int(width/8) : int(width/8) * 3]
+        if isFilled(noteCenter, 0.5):
+            filled = " Filled"
+        else:
+            filled = " Not filled"
+    else:
+        # not staffed
+        staff = " No staff"
+        noteCenter = noteImage[int(height/4) : height - int(height/4), int(width/4) : width - int(width/4)]
+        if isFilled(noteCenter, 0.5):
+            filled = " Filled"
+        else:
+            filled = " Not filled"
+        tail = " Not applicable"
+
+    return value + staff + filled + tail
