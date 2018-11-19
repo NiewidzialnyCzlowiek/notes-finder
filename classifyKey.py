32857d5819b474dac802da932c6a6f899917920e
Bartlomiej Szal <bartlomiej.szal@gmail.com>
2018-11-19 22:30:14 +0100

    Fixed bug, that occured when no contour was found

--- classifyKey.py
+++ classifyKey.py
@@ -8,15 +8,18 @@ def classifyKey(keyImage):
     keyImage = np.uint8(keyImage)
     contours = cv2.findContours(keyImage.copy(), cv2.RETR_LIST, cv2.CHAIN_APPROX_SIMPLE)
     contours = contours[0] if imutils.is_cv2() else contours[1]
-    # select contnour with biggest area
-    contour = sorted(contours, key = cv2.contourArea, reverse = True)[0]
-    # find convex hull
-    contour = cv2.convexHull(contour)
-    # calculate key height
-    top = min(contour[:,0,1])
-    bottom = max(contour[:,0,1])
-    height = bottom - top
-    if height > 80:
-        return "Violin key"
+    if len(contours) > 0:
+        # select contnour with biggest area
+        contour = sorted(contours, key = cv2.contourArea, reverse = True)[0]
+        # find convex hull
+        contour = cv2.convexHull(contour)
+        # calculate key height
+        top = min(contour[:,0,1])
+        bottom = max(contour[:,0,1])
+        height = bottom - top
+        if height > 80:
+            return "Violin key"
+        else:
+            return "Bass key"
     else:
-        return "Bass key"
+        return "Cannot classify"
