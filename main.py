1d409c31bf446fc0a4e06c7d46d825979c24a9a1
Bartlomiej Szal <bartlomiej.szal@gmail.com>
2018-11-19 18:49:01 +0100

    Added keys calssification based on contour size

--- main.py
+++ main.py
@@ -3,11 +3,12 @@ from recognizeNotes import recognizeNotes
 from extractPage import extractPage
 
 def main():
-    extracted = extractPage("./input-images/notes1.jpg")
-    cv2.imshow("extracted", extracted)
-    cv2.waitKey(0)
-    cv2.destroyAllWindows()
-    recognizeNotes(extracted)
+    extracted, success = extractPage("./input-images/notes1.jpg")
+    if success:
+        cv2.imshow("extracted", extracted)
+        cv2.waitKey(0)
+        cv2.destroyAllWindows()
+        recognizeNotes(extracted)
 
 if __name__ == "__main__":
     main()
